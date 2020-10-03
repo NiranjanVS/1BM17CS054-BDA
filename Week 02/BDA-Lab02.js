1998799bd281aff1b23de973e5f7594e7853fb21
NiranjanVS <niranjanvs.cs17@bmsce.ac.in>
2020-10-03 19:02:56 +0530

    Week 02 - Updated

--- Week 02/BDA-Lab02.js
+++ Week 02/BDA-Lab02.js
@@ -4,7 +4,7 @@ db.Employee.insert({_id:1, "EmployeeID":"HR2016056", "Name":"Niranjan", "Departm
 db.Employee.update({_id:2, "EmployeeID":"DEV2019045", "Name":"Raghavendra", "Department":"Development"},{$set:{"Age":22}},{upsert:true})
 db.Employee.save({_id:3, "EmployeeID":"REC2015098", "Name":"Obed Junias", "Department":"Recruitement", "Age":26})
 db.Employee.insert([{_id:4, "EmployeeID":"HR2017033", "Name":"Tarun M Krishna", "Department":"HR", "Age":32},{_id:5, "EmployeeID":"DEV2016029", "Name":"Shreyas K", "Department":"Development", "Age":29}])
-db.Employee.insert({_id:6, "EmployeeID":"HR2016056", "Name":"Niranjan", "Department":"HR", "Age":29})
+db.Employee.insert({_id:6, "EmployeeID":"HR2016056", "Name":"Abhishek", "Department":"HR", "Age":29})
 
 db.createCollection("Department")
 db.Department.insert({_id:1, "DeparmentID":1001, "Name":"HR", "DepartmentHead":"Abhishek"})
@@ -21,3 +21,7 @@ db.Employee.update({_id:1},{$set:{"DateOfJoining":"10/02/2019"}})
 db.Employee.update({"_id":1},{$unset:{DateOfJoining:""}})
 
 db.Department.find({"DeparmentID":{"$gte":1001,"$lte":1005}},{"Name":1,"DeparmentID":1,"_id":0});
+
+db.Employee.find({"Name":/^A/})
+
+db.Employee.find({"Age":{"$gte":30}})
